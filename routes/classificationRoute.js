4ea50e7bb3b6c11d95d8adb0a8120198219e0c02
Anna-tay <lightanna24@gmail.com>
2024-03-13 10:08:40 -0600

    to grade

--- routes/classificationRoute.js
+++ routes/classificationRoute.js
@@ -8,7 +8,8 @@ const regValidate = require('../utilities/account-validation')
 // new classification
 router.get("/new", utilities.handleErrors(classifictionController.buildNewClass))
 // manager route
-router.get("/", utilities.checkLogin, utilities.handleErrors(classifictionController.buildManager))
+router.get("/", utilities.handleErrors(classifictionController.buildManager))
+// router.get("/", utilities.checkLogin, utilities.handleErrors(classifictionController.buildManager))
 // add-inv route
 router.get("/new_inv", utilities.handleErrors(classifictionController.buildNewInv))
 
